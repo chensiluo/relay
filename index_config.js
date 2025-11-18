e0f6e851f7e69781c1b5b0422d0fa2f35bcc89b0
chensiluo <270556551@qq.com>
2025-11-18 23:03:16 +0800

    开启默认MQTT配置

--- index_config.js
+++ index_config.js
@@ -1,11 +1,11 @@
 // 默认配置
-// const DEFAULT_CONFIG = {
-//     mqtt_host: 'o6ca3537.ala.cn-hangzhou.emqxsl.cn',
-//     mqtt_port: 8084,
-//     mqtt_path: 'mqtt',
-//     mqtt_user: 'super-sms',
-//     mqtt_pwd: 'super-sms',
-//     mqtt_ssl: true
-// };
+const DEFAULT_CONFIG = {
+    mqtt_host: 'o6ca3537.ala.cn-hangzhou.emqxsl.cn',
+    mqtt_port: 8084,
+    mqtt_path: 'mqtt',
+    mqtt_user: 'super-sms',
+    mqtt_pwd: 'super-sms',
+    mqtt_ssl: true
+};
 // 当不希望使用默认配置时,可以解开下面的注释,并注释掉上面的
-const DEFAULT_CONFIG = {};
+// const DEFAULT_CONFIG = {};
